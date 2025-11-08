5dd0394aa60bb6674a02f039a26ac8e67ea171f2
Manu <manueljesusrejonsantana@gmail.com>
2025-11-07 19:40:59 -0600

    First Commit New practice TS 02-11-2025

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,4 @@
-// commitlint.config.js
-export default {
+// commitlint.config.cjs
+module.exports = {
     extends: ['@commitlint/config-conventional'],
-    // Opcional: personaliza tipos y scopes
-    rules: {
-        // Ejemplos de personalización:
-        // 'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert']],
-        // 'scope-enum': [2, 'always', ['ui', 'ux', 'deps', 'config', 'ci', 'astro', 'strapi', 'react', 'vue']],
-        // 'subject-case': [0], // permite usar emojis/texto libre en el subject
-    },
 };
